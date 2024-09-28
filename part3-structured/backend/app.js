a07ed84aef45fdede74cc883f55626bff4a7f3d8
KRISHNENDU M R <130251084+Krishnendu72@users.noreply.github.com>
2024-09-28 10:25:50 +0000

    deploy

--- part3-structured/backend/app.js
+++ part3-structured/backend/app.js
@@ -24,7 +24,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/notes', personRouter)
+app.use('/api/persons', personRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
