da89f4621d5052a32ba14b12b250b897efeaf4ea
Krishnendu M R <krishnendumr18@gmail.com>
2024-09-28 13:26:35 +0530

    uibuild

--- Part3/backend/index.js
+++ Part3/backend/index.js
@@ -30,7 +30,6 @@ const errorHandler = (error, request, response, next) => {
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
     }
-    
     next(error);
 };
 
